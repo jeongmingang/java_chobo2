1ef9604aa6e4308329874238119fe2cd5b2f09f5
mingang <j01036733521@gmail.com>
2021-02-26 12:48:47 +0900

    커밋

---
+++ src/java_chobo2/ch16/URLConnectionEx.java
@@ -0,0 +1,30 @@
+package java_chobo2.ch16;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+
+public class URLConnectionEx {
+
+	public static void main(String[] args) throws Exception {
+		URL url = new URL("https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=102&oid=023&aid=0003598407");
+		
+		try(BufferedReader br = new BufferedReader(
+				new InputStreamReader(url.openStream()));
+				BufferedWriter bw = new BufferedWriter(
+						new OutputStreamWriter(
+								new FileOutputStream("test.html")))){
+						
+			String line = "";
+			
+			while( (line = br.readLine()) != null) {
+				System.out.println(line);
+				bw.write(line);
+			}
+		}
+	}
+}
