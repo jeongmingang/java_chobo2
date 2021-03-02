291f334f853c489c80505bfdc2d118e49aca0fc8
mingang <j01036733521@gmail.com>
2021-03-02 10:23:00 +0900

    lambda 예제 추가

--- src/java_chobo2/ch14/LambdaEx01.java
+++ src/java_chobo2/ch14/LambdaEx01.java
@@ -1,29 +1,29 @@
 package java_chobo2.ch14;
 
- interface LambaInter {
+ interface LambdaInter {
 	int max(int a, int b);
 }
 
-class lambdaInterClass implements LambaInter {
+class lambdaInterClass implements LambdaInter {
 	@Override
 	public int max(int a, int b) {
 		return a > b ? a : b;
 	}
 }
 
-interface LambaInter1 {
+interface LambdaInter1 {
 	void printVar(String name, int i);
 }
 
-interface LambaInter2 {	// 인터페이스의 선언은 하나만 가능
+interface LambdaInter2 {	// 인터페이스의 선언은 하나만 가능
 	int square(int x);
 }
 
-interface LambaInter3 {	
+interface LambdaInter3 {	
 	int roll();
 }
 
-interface LambaInter4 {	
+interface LambdaInter4 {	
 	int sumArr(int[] arr);
 }
 
@@ -39,31 +39,38 @@ public class LambdaEx01 {
 		int c = lic.max(5, 4);
 		System.out.println("max = " + c);
 		
-		LambaInter lbi = (i, j) -> i > j ? i : j;	// 두개의 변수가 i,j로 선언, lbl로 사용가능
+//		LambdaInter lbi10 = new lambdaInter() {
+//		@Override
+//		public int max(int a, int b) {
+//			return a > b ? a : b;
+//		}
+//	};
+		
+		LambdaInter lbi = (i, j) -> i > j ? i : j;	// 두개의 변수가 i,j로 선언, lbl로 사용가능
 		int b = lbi.max(5, 3);
 		System.out.println("max = " + b);
 	
 	
-		LambaInter1 lbi1 = (String name, int i) -> {System.out.println(name + " : " + i);};
+		LambdaInter1 lbi1 = (String name, int i) -> {System.out.println(name + " : " + i);};
 		lbi1.printVar("김상건", 5);
 		
-		LambaInter1 lbi2 = (name, i) -> {System.out.println(name + " : " + i);};
+		LambdaInter1 lbi2 = (name, i) -> {System.out.println(name + " : " + i);};
 		lbi2.printVar("이나연", 15);
 		
-		LambaInter1 lbi3 = (name, i) -> System.out.println(name + " : " + i);
+		LambdaInter1 lbi3 = (name, i) -> System.out.println(name + " : " + i);
 		lbi3.printVar("신범건", 25);
 		
-		LambaInter2 lbi4 = (int x) -> x * x;
-		LambaInter2 lbi5 = x -> x * x;
+		LambdaInter2 lbi4 = (int x) -> x * x;
+		LambdaInter2 lbi5 = x -> x * x;
 		System.out.println("square : " + lbi4.square(5));
 		System.out.println("square : " + lbi5.square(5));
 		
-		LambaInter3 lbi6 = () -> {return (int)(Math.random()*6);}; // (0~5)
-		LambaInter3 lbi7 = () -> (int)(Math.random()*6);
+		LambdaInter3 lbi6 = () -> {return (int)(Math.random()*6);}; // (0~5)
+		LambdaInter3 lbi7 = () -> (int)(Math.random()*6);
 		System.out.println("random : " + lbi6.roll());
 		System.out.println("random : " + lbi7.roll());
 		
-		LambaInter4 lbi8 = (int[] arr)-> {
+		LambdaInter4 lbi8 = (int[] arr)-> {
 			int sum = 0;
 			for(int i : arr) sum+=i;
 			return sum;
