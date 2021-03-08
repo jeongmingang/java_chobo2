29f59afcefcde4fe2d687fee6ff0e99a78a99332
mingang <j01036733521@gmail.com>
2021-03-08 09:52:11 +0900

    최종연산 matchEx, findEx

---
+++ src/java_chobo2/ch14/stream/StreamConditionEx.java
@@ -0,0 +1,57 @@
+package java_chobo2.ch14.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class StreamConditionEx {
+
+	public static void main(String[] args) {
+		List<Student> list = new ArrayList<>();
+		list.add(new Student("이자바", 3, 300));
+		list.add(new Student("김자바", 1, 200));
+		list.add(new Student("안자바", 2, 100));
+		list.add(new Student("박자바", 2, 150));
+		list.add(new Student("소자바", 1, 95));
+		list.add(new Student("나자바", 3, 290));
+		list.add(new Student("감자바", 3, 80));
+		
+		matchEx(list);
+		
+		findEx(list);
+	}
+
+	private static void findEx(List<Student> list) {
+		Stream<Student> stdStream = list.stream();
+		
+		Optional<Student> result = stdStream.filter(s -> s.getTotalscore() <= 100).findFirst();
+		System.out.println(result);
+		
+		result = list.parallelStream().filter(s -> s.getTotalscore() <= 100).findAny();
+		System.out.println(result);
+	}
+
+	private static void matchEx(List<Student> list) {
+		Stream<Student> stdStream = list.stream();
+		
+		boolean hasRes = stdStream.anyMatch(new Predicate<Student>() {
+			@Override
+			public boolean test(Student t) {
+				return t.getTotalscore() <= 100;
+			}
+		});
+		
+		System.out.printf("hasRes => %s%n", hasRes);
+		
+		stdStream = list.stream();					// 스트림 소모로 다시 생성
+		hasRes = stdStream.allMatch(new Predicate<Student>() {	// 나중에 확인
+			@Override
+			public boolean test(Student t) {
+				return t.getTotalscore() <= 80;
+			}
+	});
+		System.out.printf("getTotalScore => %s%n", hasRes);
+}
+}
